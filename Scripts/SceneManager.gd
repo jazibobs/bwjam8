1f91a12e08c3e722141756f205924600bce11955
Jared Rigby <jared@mtddigital.uk>
2022-04-05 20:16:11 +0800

    Initial commit

---
+++ Scripts/SceneManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var current_scene
+var next_scene
+
+func change_scene(old_scene, new_scene):
+	current_scene = old_scene
+	next_scene = new_scene
+	$AnimationPlayer.play("scene_fade_out")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "scene_fade_out":
+		current_scene.queue_free()
+		get_parent().add_child(next_scene)
+		$AnimationPlayer.play("scene_fade_in")
